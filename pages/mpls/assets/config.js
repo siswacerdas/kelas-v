ac89408e7be4d98e6a60500b96acd22454bfaa67
Arif Azwar Anas <arif.azwar79@gmail.com>
2026-09-01 06:48:43 +0700

    Update config.js

--- pages/mpls/assets/config.js
+++ pages/mpls/assets/config.js
@@ -15,6 +15,6 @@
  *    sudah dipakai di halaman utama begitu sudah aktif.
  */
 const MPLS_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzHxuJtOggcVQ4WO54AgNfKK6A4fSAm8WVksNnvVVKaTuc4C_GkIcdekhKZIWQ8FhSY3w/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzF3Ln0L8rOkAl48YsJKeXCiV7CUS8mu37xAyIMQUdkFf1puCiOInHyA0ONyXwkYJlWdA/exec",
   ACCESS_CODE: "mpls2026",
 };
